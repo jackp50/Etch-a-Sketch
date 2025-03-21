22268752482cc2afc904a761b57e485e04796d33
jackp50 <jpearne9@gmail.com>
2025-03-20 18:35:39 -0600

    Added hover effect

--- script.js
+++ script.js
@@ -6,5 +6,11 @@ for (let i = 1; i <= 256; i++) {
     // Create a new div
     const gridDiv = document.createElement("div");
     //append new div to parent container div
-    container.appendChild(gridDiv); // Append the div to the container
+    container.appendChild(gridDiv); 
+
+    // hover effect using mouseenter
+    gridDiv.addEventListener("mouseenter", () => {
+        //background color for hover effect (has to be in eventListener)
+        gridDiv.style.backgroundColor = "aqua";
+    });
 }
