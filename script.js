55aeab42d84a1c987ecaf3b0eeb92349a2b77268
jackp50 <jpearne9@gmail.com>
2025-03-20 20:01:35 -0600

    Basic version of Etch-a-Sketch working, I am going to keep updated to make the site look much more professonal

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const container = document.querySelector("#container");
 
 //buildGrid function
 function buildGrid(gridSize) {
+    //clear the inside everytime
+    container.innerHTML = "";
     //each square will be the max height and width divided the users choice
     const squares = 960 / gridSize;
 
@@ -15,6 +17,8 @@ function buildGrid(gridSize) {
             gridDiv.style.width = `${squares}px`;
             gridDiv.style.height = `${squares}px`;
             gridDiv.style.position = "absolute"
+            gridDiv.style.top = `${row * squares}px`;
+            gridDiv.style.left = `${column * squares}px`;
             //append new div to parent
             container.appendChild(gridDiv); 
             // hover effect using mouseenter
@@ -25,6 +29,22 @@ function buildGrid(gridSize) {
         }
     }
 }
+
+
+// creating a button
+const button = document.createElement("button");
+button.textContent = "Grid Size";
+document.body.insertBefore(button, container);
+
+// adding a click effect to that button
+button.addEventListener("click", () => {
+    const gridSize = parseInt(prompt("Enter the number of squares per side"));
+    if (gridSize > 0 && gridSize <= 100) {
+        buildGrid(gridSize);
+    } else {
+        alert("Please enter a valid number between 1 and 100.");
+    }
+});
 
 
 
