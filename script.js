e668eda83dca3d2c5aa49becf906d2fd3d6b0d13
jackp50 <jpearne9@gmail.com>
2025-03-20 18:11:05 -0600

    After realizing I needed 256 girds instead of 32 I scrapped the whole script page and redid the grid building but this time with a loop... much easier

--- script.js
+++ script.js
@@ -1,77 +1,10 @@
-//main container created in html file
+// the container that was made in the html file
 const container = document.querySelector("#container");
 
-//div creation for 16 x 16 grid
-const gridOne = document.createElement("div");
-const gridTwo = document.createElement("div");
-const gridThree = document.createElement("div");
-const gridFour = document.createElement("div");
-const gridFive = document.createElement("div");
-const gridSix = document.createElement("div");
-const gridSeven = document.createElement("div");
-const gridEight = document.createElement("div");
-const gridNine = document.createElement("div");
-const gridTen = document.createElement("div");
-const gridEleven = document.createElement("div");
-const gridTwelve = document.createElement("div");
-const gridThirteen = document.createElement("div");
-const gridFourteen = document.createElement("div");
-const gridFifteen = document.createElement("div");
-const gridSixteen = document.createElement("div");
-const gridSeventeen = document.createElement("div");
-const gridEighteen = document.createElement("div");
-const gridNineteen = document.createElement("div");
-const gridTwenty = document.createElement("div");
-const gridTwentyOne = document.createElement("div");
-const gridTwentyTwo = document.createElement("div");
-const gridTwentyThree = document.createElement("div");
-const gridTwentyFour = document.createElement("div");
-const gridTwentyFive = document.createElement("div");
-const gridTwentySix = document.createElement("div");
-const gridTwentySeven = document.createElement("div");
-const gridTwentyEight = document.createElement("div");
-const gridTwentyNine = document.createElement("div");
-const gridThirty = document.createElement("div");
-const gridThirtyOne = document.createElement("div");
-const gridThirtyTwo = document.createElement("div");
-
-container.appendChild(gridOne);
-container.appendChild(gridTwo);
-container.appendChild(gridThree);
-container.appendChild(gridFour);
-container.appendChild(gridFive);
-container.appendChild(gridSix);
-container.appendChild(gridSeven);
-container.appendChild(gridEight);
-container.appendChild(gridNine);
-container.appendChild(gridTen);
-container.appendChild(gridEleven);
-container.appendChild(gridTwelve);
-container.appendChild(gridThirteen);
-container.appendChild(gridFourteen);
-container.appendChild(gridFifteen);
-container.appendChild(gridSixteen);
-container.appendChild(gridSeventeen);
-container.appendChild(gridEighteen);
-container.appendChild(gridNineteen);
-container.appendChild(gridTwenty);
-container.appendChild(gridTwentyOne);
-container.appendChild(gridTwentyTwo);
-container.appendChild(gridTwentyThree);
-container.appendChild(gridTwentyFour);
-container.appendChild(gridTwentyFive);
-container.appendChild(gridTwentySix);
-container.appendChild(gridTwentySeven);
-container.appendChild(gridTwentyEight);
-container.appendChild(gridTwentyNine);
-container.appendChild(gridThirty);
-container.appendChild(gridThirtyOne);
-container.appendChild(gridThirtyTwo);
-
-
-
-
-
-
-
-
+// Loop to create a 16x16 div squares
+for (let i = 1; i <= 256; i++) {
+    // Create a new div
+    const gridDiv = document.createElement("div");
+    //append new div to parent container div
+    container.appendChild(gridDiv); // Append the div to the container
+}
