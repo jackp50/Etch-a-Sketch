d0acffd99fa0b62316bae1e06e4154682d5c1be5
jackp50 <jpearne9@gmail.com>
2025-03-20 19:31:57 -0600

    Deleted some css and added it into js styling. In the process of changing script.js so that the grid is created by user input. Just finished the nested loop that will create such a thing

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 // the container that was made in the html file
 const container = document.querySelector("#container");
 
-// Loop to create a 16x16 div squares
-for (let i = 1; i <= 256; i++) {
-    // Create a new div
-    const gridDiv = document.createElement("div");
-    //append new div to parent container div
-    container.appendChild(gridDiv); 
 
-    // hover effect using mouseenter
-    gridDiv.addEventListener("mouseenter", () => {
-        //background color for hover effect (has to be in eventListener)
-        gridDiv.style.backgroundColor = "aqua";
-    });
+//buildGrid function
+function buildGrid(gridSize) {
+    //each square will be the max height and width divided the users choice
+    const squares = 960 / gridSize;
+
+    //using a nested loop to create rows and columns
+    for (row = 0; row < gridSize; row++) {
+        for(column = 0; column < gridSize; column++) {
+            //create a new div
+            const gridDiv = document.createElement("div");
+            gridDiv.style.width = `${squares}px`;
+            gridDiv.style.height = `${squares}px`;
+            gridDiv.style.position = "absolute"
+            //append new div to parent
+            container.appendChild(gridDiv); 
+            // hover effect using mouseenter
+            gridDiv.addEventListener("mouseenter", () => {
+            //background color for hover effect (has to be in eventListener)
+            gridDiv.style.backgroundColor = "aqua";
+            });
+        }
+    }
 }
+
+
+
